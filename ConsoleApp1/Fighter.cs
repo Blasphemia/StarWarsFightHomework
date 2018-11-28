bf3b65d2b794cdc4e49ecea9d80f30d12b5c933e
deathka1990 <deathka1990@gmail.com>
2018-11-28 15:09:26 +0300

    Add Classes

---
+++ ConsoleApp1/Fighter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class Fighter
+    {
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Power { get; set; }
+        public string Name { get; set; }
+
+
+        
+    }
+}
---
+++ ConsoleApp1/Fraction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Fraction
+    {
+        public string Name { get; set; }
+        public List<Squad> Squads { get; set; }
+    }
+}
